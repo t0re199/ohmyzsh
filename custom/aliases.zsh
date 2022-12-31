f8b26427427e0f7d37e03e30956cd7a3fa6dcc00
t0re199 <t0re199@t0re199-MBP.local>
2022-12-31 12:45:04 +0100

    Aliases Added

---
+++ custom/aliases.zsh
@@ -0,0 +1,10 @@
+# t0re199 x git aliases
+alias gs='git stage'
+alias gc='git commit'
+alias gp='git push'
+alias gmc='gs . && gc -m Minor\ Changes && gp'
+
+# t0re199 x file system aliases
+alias cb='cd /Users/t0re199/git/bitbucket'
+alias cg='cd /Users/t0re199/git/github'
+alias bb='cd ..'
